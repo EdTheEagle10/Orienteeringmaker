b6408d363da532f5c3c5e95bffa6342a41df4de9
EdTheEagle10 <edvardkristoffer10@gmail.com>
2026-05-13 20:09:55 +0200

    Update map_analyzer.py

--- map_analyzer.py
+++ map_analyzer.py
@@ -3,9 +3,10 @@ import numpy as np
 import random 
 
 class MapAnalyzer:
-    def __init__(self, bilde, spraak="NO"):
+    def __init__(self, bilde, spraak="NO", blokk_maske=None):
         self.bilde = bilde
         self.spraak = spraak
+        self.blokk_maske = blokk_maske  # NEW: Custom user mask
         self.hsv = cv2.cvtColor(self.bilde, cv2.COLOR_BGR2HSV)
         self.cost_map = None
         self.lovlige_detaljer = [] 
@@ -34,6 +35,34 @@ class MapAnalyzer:
                             funn.append((x, y))
         return funn
 
+    def _finn_komplekse_detaljer(self, mask_svart, forbudt_sone):
+        taarn, kunstig = [], []
+        konturer, hierarki = cv2.findContours(mask_svart, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        if hierarki is not None:
+            hierarki = hierarki[0]
+            for i, k in enumerate(konturer):
+                areal = cv2.contourArea(k)
+                if 15 < areal < 250: 
+                    M = cv2.moments(k)
+                    if M['m00'] != 0:
+                        x, y = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
+                        if forbudt_sone[y, x] == 0:
+                            har_barn = hierarki[i][2] != -1
+                            omkrets = cv2.arcLength(k, True)
+                            sirkularitet = (4 * np.pi * areal) / (omkrets * omkrets) if omkrets > 0 else 0
+                            
+                            if har_barn and sirkularitet > 0.5:
+                                taarn.append((x, y))
+                                continue
+
+                            hull = cv2.convexHull(k)
+                            hull_areal = cv2.contourArea(hull)
+                            solidity = areal / float(hull_areal) if hull_areal > 0 else 0
+                            if 0.2 < solidity < 0.65:
+                                kunstig.append((x, y))
+        return taarn, kunstig
+
     def _analyser_kart(self):
         h, w = self.bilde.shape[:2]
         self.cost_map = np.full((h, w), 5000.0, dtype=np.float32)
@@ -55,19 +84,11 @@ class MapAnalyzer:
         self.cost_map[mask_oliven > 0] = 5000.0 
         
         mask_brun = cv2.inRange(self.hsv, np.array([10, 100, 50]), np.array([25, 255, 200]))
-        self.cost_map[mask_brun > 0] += 5.0 # Vanlig kurvekryssing koster litt
+        self.cost_map[mask_brun > 0] += 5.0
 
-        # --- NYTT: ANTI-BRATT FJELLSIDE (TETTHET AV HØYDEKURVER) ---
-        # Vi legger en enorm uskarphet over kurvene. Tette kurver smelter sammen til store flekker.
         tetthet_kurver = cv2.GaussianBlur(mask_brun, (21, 21), 0)
-        
-        # Der kurvene smelter sammen (høy tetthet), lager vi en "bratt-maske"
         mask_bratt = cv2.threshold(tetthet_kurver, 40, 255, cv2.THRESH_BINARY)[1]
-        
-        # Vi gir bratte områder en enorm straff (f.eks. +150 i kostnad per piksel)
-        # Dette tvinger stifinneren til å velge slakere veier rundt fjellet!
         self.cost_map[mask_bratt > 0] += 150.0 
-        # ------------------------------------------------------------
 
         self.mask_kurver = mask_brun.copy()
 
@@ -85,16 +106,28 @@ class MapAnalyzer:
                 cv2.drawContours(mask_hav, [k], -1, 255, -1)
 
         mask_forbudt = cv2.bitwise_or(mask_oliven, mask_hav)
+
+        # --- NEW: APPLY THE CUSTOM BLOCK MASK FROM THE USER ---
+        if self.blokk_maske is not None:
+            mask_forbudt = cv2.bitwise_or(mask_forbudt, self.blokk_maske)
+            self.cost_map[self.blokk_maske > 0] = 5000.0
+
         forbudt_sone_poster = cv2.dilate(mask_forbudt, np.ones((40,40), np.uint8), iterations=1)
 
-        steiner = self._finn_detaljer_i_maske(mask_svart_detaljer, forbudt_sone_poster, 4, 100, 0.6)
+        steiner = self._finn_detaljer_i_maske(mask_svart_detaljer, forbudt_sone_poster, 15, 100, 0.75)
         skrenter = self._finn_detaljer_i_maske(mask_svart_detaljer, forbudt_sone_poster, 15, 200, 0.0)
+        bygninger = self._finn_detaljer_i_maske(mask_svart_detaljer, forbudt_sone_poster, 100, 800, 0.2)
         brune_detaljer = self._finn_detaljer_i_maske(mask_brun_detaljer, forbudt_sone_poster, 4, 100, 0.3)
         blaa_detaljer = self._finn_detaljer_i_maske(mask_blaa, forbudt_sone_poster, 4, 80, 0.4)
+        
+        taarn, kunstig_detalj = self._finn_komplekse_detaljer(mask_svart_detaljer, forbudt_sone_poster)
+        
+        mask_gronn_detaljer = cv2.morphologyEx(mask_gronn, cv2.MORPH_OPEN, kernel_tekst)
+        sermerkede_traer = self._finn_detaljer_i_maske(mask_gronn_detaljer, forbudt_sone_poster, 10, 80, 0.70)
 
         mask_stier = mask_svart.copy()
-        for x, y in steiner + skrenter:
-            cv2.circle(mask_stier, (x, y), 8, 0, -1)
+        for x, y in steiner + skrenter + bygninger + taarn + kunstig_detalj:
+            cv2.circle(mask_stier, (x, y), 10, 0, -1)
             
         kryss_punkter = cv2.goodFeaturesToTrack(mask_stier, maxCorners=150, qualityLevel=0.1, minDistance=30)
         stikryss = []
@@ -105,18 +138,22 @@ class MapAnalyzer:
                     stikryss.append((x, y))
 
         trans = {
-            "NO": {"stein": "Stein", "skrent": "Skrent", "grop": "Grop", "vann": "Vannhull", "kryss": "Stikryss", "y": "Y-kryss",
+            "NO": {"stein": "Stein", "skrent": "Skrent", "grop": "Grop", "kolle": "Kolle", "bygning": "Bygning", "vann": "Vannhull", "kryss": "Stikryss", "y": "Y-kryss",
+                   "taarn": "Tårn", "kunstig": "Kunstig detalj", "tre": "Særmerket tre",
                    "n": "Nordre", "s": "Søndre", "e": "Østre", "w": "Vestre", "nw": "Nordvestre", "ne": "Nordøstre", "sw": "Sørvestre", "se": "Sørøstre",
-                   "side": "side", "fot": "Foten", "ovre": "Øvre kant", "kant": "kant", "ikryss": "I krysset"},
-            "EN": {"stein": "Boulder", "skrent": "Earth bank", "grop": "Pit", "vann": "Waterhole", "kryss": "Path crossing", "y": "Path junction",
+                   "side": "side", "fot": "Foten", "ovre": "Øvre kant", "kant": "kant", "hjorne": "hjørne", "ikryss": "I krysset"},
+            "EN": {"stein": "Boulder", "skrent": "Earth bank", "grop": "Pit", "kolle": "Knoll", "bygning": "Building", "vann": "Waterhole", "kryss": "Path crossing", "y": "Path junction",
+                   "taarn": "Tower", "kunstig": "Man-made feature", "tre": "Prominent tree",
                    "n": "Northern", "s": "Southern", "e": "Eastern", "w": "Western", "nw": "North-Western", "ne": "North-Eastern", "sw": "South-Western", "se": "South-Eastern",
-                   "side": "side", "fot": "Foot", "ovre": "Upper part", "kant": "edge", "ikryss": "In junction"},
-            "SV": {"stein": "Sten", "skrent": "Brant", "grop": "Grop", "vann": "Vattenhål", "kryss": "Stigkorsning", "y": "Stigförgrening",
+                   "side": "side", "fot": "Foot", "ovre": "Upper part", "kant": "edge", "hjorne": "corner", "ikryss": "In junction"},
+            "SV": {"stein": "Sten", "skrent": "Brant", "grop": "Grop", "kolle": "Höjd", "bygning": "Byggnad", "vann": "Vattenhål", "kryss": "Stigkorsning", "y": "Stigförgrening",
+                   "taarn": "Torn", "kunstig": "Konstgjord detalj", "tre": "Särmärkt träd",
                    "n": "Norra", "s": "Södra", "e": "Östra", "w": "Västra", "nw": "Nordvästra", "ne": "Nordöstra", "sw": "Sydvästra", "se": "Sydöstra",
-                   "side": "sida", "fot": "Foten", "ovre": "Övre kanten", "kant": "kant", "ikryss": "I korsningen"},
-            "DE": {"stein": "Felsblock", "skrent": "Böschung", "grop": "Senke", "vann": "Wasserloch", "kryss": "Wegekreuzung", "y": "Wegabzweigung",
+                   "side": "sida", "fot": "Foten", "ovre": "Övre kanten", "kant": "kant", "hjorne": "hörn", "ikryss": "I korsningen"},
+            "DE": {"stein": "Felsblock", "skrent": "Böschung", "grop": "Senke", "kolle": "Kuppe", "bygning": "Gebäude", "vann": "Wasserloch", "kryss": "Wegekreuzung", "y": "Wegabzweigung",
+                   "taarn": "Turm", "kunstig": "Künstliches Objekt", "tre": "Auffälliger Baum",
                    "n": "Nord", "s": "Süd", "e": "Ost", "w": "West", "nw": "Nordwest", "ne": "Nordost", "sw": "Südwest", "se": "Südost",
-                   "side": "Seite", "fot": "Fuß", "ovre": "Oberer Teil", "kant": "Rand", "ikryss": "An der Kreuzung"}
+                   "side": "Seite", "fot": "Fuß", "ovre": "Oberer Teil", "kant": "Rand", "hjorne": "Ecke", "ikryss": "An der Kreuzung"}
         }
         t = trans.get(self.spraak, trans["NO"])
         retninger = ["n", "s", "e", "w", "nw", "ne", "sw", "se"]
@@ -128,9 +165,29 @@ class MapAnalyzer:
         for p in skrenter:
             r = random.choice(["fot", "ovre"])
             self.detalj_typer[p] = {"sym": "skrent", "ret": r, "tekst": f"{t['skrent']}, {t[r]}"}
+        for p in bygninger:
+            r = random.choice(["nw", "ne", "sw", "se"]) 
+            self.detalj_typer[p] = {"sym": "bygning", "ret": r.upper(), "tekst": f"{t['bygning']}, {t[r]} {t['hjorne']}"}
+        
+        for p in taarn:
+            r = random.choice(["n", "s", "e", "w"]) 
+            self.detalj_typer[p] = {"sym": "taarn", "ret": r.upper(), "tekst": f"{t['taarn']}, {t[r]} {t['side']}"}
+        for p in kunstig_detalj:
+            r = random.choice(["n", "s", "e", "w"]) 
+            self.detalj_typer[p] = {"sym": "kunstig", "ret": r.upper(), "tekst": f"{t['kunstig']}, {t[r]} {t['side']}"}
+        for p in sermerkede_traer:
+            r = random.choice(["n", "s", "e", "w"]) 
+            self.detalj_typer[p] = {"sym": "tre", "ret": r.upper(), "tekst": f"{t['tre']}, {t[r]} {t['side']}"}
+
         for p in brune_detaljer:
-            r = random.choice(retninger)
-            self.detalj_typer[p] = {"sym": "grop", "ret": r.upper(), "tekst": f"{t['grop']}, {t[r]} {t['kant']}"}
+            er_kolle = random.choice([True, False])
+            if er_kolle:
+                r = random.choice(["n", "s", "e", "w"])
+                self.detalj_typer[p] = {"sym": "kolle", "ret": r.upper(), "tekst": f"{t['kolle']}, {t[r]} {t['fot']}"}
+            else:
+                r = random.choice(["n", "s", "e", "w"])
+                self.detalj_typer[p] = {"sym": "grop", "ret": r.upper(), "tekst": f"{t['grop']}, {t[r]} {t['kant']}"}
+                
         for p in blaa_detaljer:
             r = random.choice(retninger)
             self.detalj_typer[p] = {"sym": "vann", "ret": r.upper(), "tekst": f"{t['vann']}, {t[r]} {t['kant']}"}
@@ -143,3 +200,5 @@ class MapAnalyzer:
         for x, y in self.lovlige_detaljer: cv2.circle(ledelinjer, (x, y), 5, 0, -1) 
         invertert = cv2.bitwise_not(ledelinjer)
         self.avstand_til_sti = cv2.distanceTransform(invertert, cv2.DIST_L2, 5)
+
+    
