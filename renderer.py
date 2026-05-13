66f6a11a3e48e2e57fd8c3fafd52d8b8842cb3cc
EdTheEagle10 <edvardkristoffer10@gmail.com>
2026-05-13 20:11:17 +0200

    Update renderer.py

--- renderer.py
+++ renderer.py
@@ -7,7 +7,7 @@ from course_generator import RouteAnalyzer
 class MapRenderer:
     def __init__(self, bilde, spraak="NO"):
         self.bilde = bilde
-        self.o_farge = (143, 0, 200) 
+        self.o_farge = (143, 0, 200) # Lilla IOF-farge
         self.spraak = spraak
         
         self.T = {
@@ -55,6 +55,7 @@ class MapRenderer:
 
         valgt_font = font_bold if tykkelse > 1 else font
 
+        # Hvit outline rundt teksten
         if halo:
             o_range = 2 if tykkelse > 1 else 1
             for dx in range(-o_range, o_range+1):
@@ -95,29 +96,44 @@ class MapRenderer:
                 
         cv2.addWeighted(overlay, 0.5, self.bilde, 0.5, 0, self.bilde)
 
+        # --- NYTT: PURPLE PEN HALO-EFFEKT ---
         loype_lag = np.zeros_like(self.bilde)
+        halo_lag = np.zeros_like(self.bilde)
+
+        # Tegner hvite bakgrunner først
         for i in range(len(loype) - 1):
             k1, k2 = self.finn_kant_punkt(loype[i], loype[i+1], radius, radius)
+            cv2.line(halo_lag, k1, k2, (255,255,255), 9)
             cv2.line(loype_lag, k1, k2, self.o_farge, 3)
 
         s_cx, s_cy = loype[0]
         pts = np.array([[s_cx, s_cy-22], [s_cx-20, s_cy+15], [s_cx+20, s_cy+15]], np.int32)
+        cv2.polylines(halo_lag, [pts], True, (255,255,255), 10)
         cv2.polylines(loype_lag, [pts], True, self.o_farge, 4)
 
         poster = loype[1:-1]
         for idx, p in enumerate(poster):
+            cv2.circle(halo_lag, p, radius, (255,255,255), 10)
             cv2.circle(loype_lag, p, radius, self.o_farge, 4)
 
         maal = loype[-1]
+        cv2.circle(halo_lag, maal, radius-5, (255,255,255), 10)
+        cv2.circle(halo_lag, maal, radius+5, (255,255,255), 10)
         cv2.circle(loype_lag, maal, radius-5, self.o_farge, 4)
         cv2.circle(loype_lag, maal, radius+5, self.o_farge, 4)
 
+        # Smelter den hvite haloen inn i kartet
+        mask_halo = cv2.cvtColor(halo_lag, cv2.COLOR_BGR2GRAY)
+        self.bilde[mask_halo > 0] = halo_lag[mask_halo > 0]
+
+        # Sørger for at lilla ikke dekker over svarte steiner/skrenter
         hsv = cv2.cvtColor(self.bilde, cv2.COLOR_BGR2HSV)
         mask_mork = cv2.inRange(hsv, np.array([0, 0, 0]), np.array([180, 255, 90]))
         halo_kernel = np.ones((4,4), np.uint8)
         mask_klipp = cv2.dilate(mask_mork, halo_kernel, iterations=1)
         loype_lag[mask_klipp > 0] = [0, 0, 0]
 
+        # Legger på selve løypa
         mask_ferdig = cv2.cvtColor(loype_lag, cv2.COLOR_BGR2GRAY)
         self.bilde[mask_ferdig > 0] = loype_lag[mask_ferdig > 0]
 
@@ -127,6 +143,7 @@ class MapRenderer:
             self.tegn_tekst(kode_tekst, (p[0]+25, p[1]-15), 0.9, self.o_farge, tykkelse=2, halo=True)
         self.bruk_tekst_lag()
 
+    # --- IOF SYMBOLER ---
     def tegn_iof_stein(self, img, x, y, sc=1.0):
         s = int(8 * sc)
         pts = np.array([[x, y-s], [x-s, y+s], [x+s, y+s]], np.int32)
@@ -141,8 +158,7 @@ class MapRenderer:
         cv2.line(img, (x+s8, y-s5), (x+s8, y+s5), (0,0,0), th2)
 
     def tegn_iof_grop(self, img, x, y, sc=1.0):
-        s6 = int(6*sc)
-        th = max(1, int(3*sc))
+        s6 = int(6*sc); th = max(1, int(3*sc))
         cv2.line(img, (x-s6, y-s6), (x, y+s6), (0, 75, 150), th)
         cv2.line(img, (x+s6, y-s6), (x, y+s6), (0, 75, 150), th)
 
@@ -150,8 +166,7 @@ class MapRenderer:
         cv2.circle(img, (x, y), max(2, int(6*sc)), (255, 0, 0), -1)
 
     def tegn_iof_kryss(self, img, x, y, er_y=False, sc=1.0):
-        s8 = int(8*sc); s6 = int(6*sc)
-        th = max(1, int(2*sc))
+        s8 = int(8*sc); s6 = int(6*sc); th = max(1, int(2*sc))
         if er_y:
             cv2.line(img, (x, y), (x, y+s8), (0,0,0), th)
             cv2.line(img, (x, y), (x-s6, y-s6), (0,0,0), th)
@@ -159,6 +174,28 @@ class MapRenderer:
         else:
             cv2.line(img, (x-s8, y), (x+s8, y), (0,0,0), th)
             cv2.line(img, (x, y-s8), (x, y+s8), (0,0,0), th)
+
+    def tegn_iof_bygning(self, img, x, y, sc=1.0):
+        s = int(6 * sc)
+        pts = np.array([[x-s, y-s], [x-s, y+s], [x+s, y+s], [x+s, y-s]], np.int32)
+        cv2.fillPoly(img, [pts], (0,0,0))
+
+    def tegn_iof_kolle(self, img, x, y, sc=1.0):
+        cv2.circle(img, (x, y), max(2, int(4*sc)), (0, 75, 150), -1)
+
+    def tegn_iof_taarn(self, img, x, y, sc=1.0):
+        cv2.circle(img, (x, y), max(3, int(6*sc)), (0,0,0), max(1, int(2*sc)))
+
+    def tegn_iof_kunstig(self, img, x, y, sc=1.0):
+        s = int(5*sc); th = max(1, int(2*sc))
+        cv2.line(img, (x-s, y-s), (x+s, y+s), (0,0,0), th)
+        cv2.line(img, (x-s, y+s), (x+s, y-s), (0,0,0), th)
+
+    def tegn_iof_tre(self, img, x, y, sc=1.0):
+        s = int(5*sc); th = max(1, int(2*sc))
+        pts = np.array([[x, y-s], [x-s, y+s-2], [x+s, y+s-2]], np.int32)
+        cv2.polylines(img, [pts], True, (0,0,0), th)
+        cv2.line(img, (x, y+s-2), (x, y+s+2), (0,0,0), th)
 
     def tegn_iof_retning(self, img, x, y, ret_id, sc=1.0):
         dx, dy = 0, 0
@@ -215,8 +252,7 @@ class MapRenderer:
             min_avstand = min(min_avstand, math.hypot(dx, dy))
 
         skala = 1.0
-        if min_avstand < 80:
-            skala = 0.65 
+        if min_avstand < 80: skala = 0.65 
 
         celle_w = int(celle_w_base * skala)
         celle_h = int(celle_h_base * skala)
@@ -234,7 +270,6 @@ class MapRenderer:
             cv2.line(self.bilde, (start_x + k_x, start_y), (start_x + k_x, start_y + boks_h), self.o_farge, max(1, int(2*skala)))
 
         self.start_tekst_lag()
-        
         overskrift = f"{t['len']}: {lengde_km:.2f} km | {t['climb']}: {int(stigning_m)} m"
         self.tegn_tekst(overskrift, (start_x, start_y - int(10*skala)), 0.8 * skala, self.o_farge, 2)
 
@@ -245,8 +280,9 @@ class MapRenderer:
             self.tegn_tekst(nr_str, (start_x + int(15*skala), y_senter + int(8*skala)), font_skala, (0,0,0), 2)
 
             if 0 < i < len(loype) - 1:
-                kode = str(koder[i-1])
-                self.tegn_tekst(kode, (start_x + celle_w + int(5*skala), y_senter + int(8*skala)), font_skala * 0.9, (0,0,0), 2)
+                if (i-1) < len(koder):
+                    kode = str(koder[i-1])
+                    self.tegn_tekst(kode, (start_x + celle_w + int(5*skala), y_senter + int(8*skala)), font_skala * 0.9, (0,0,0), 2)
 
                 info = detalj_typer.get(punkt, {"sym": "", "ret": "", "tekst": ""})
                 tekst = info["tekst"]
@@ -266,9 +302,14 @@ class MapRenderer:
                 if sym == "stein": self.tegn_iof_stein(self.bilde, x_sym3, y_senter, sc=skala)
                 elif sym == "skrent": self.tegn_iof_skrent(self.bilde, x_sym3, y_senter, sc=skala)
                 elif sym == "grop": self.tegn_iof_grop(self.bilde, x_sym3, y_senter, sc=skala)
+                elif sym == "kolle": self.tegn_iof_kolle(self.bilde, x_sym3, y_senter, sc=skala)
+                elif sym == "bygning": self.tegn_iof_bygning(self.bilde, x_sym3, y_senter, sc=skala)
                 elif sym == "vann": self.tegn_iof_vannhull(self.bilde, x_sym3, y_senter, sc=skala)
                 elif sym == "y": self.tegn_iof_kryss(self.bilde, x_sym3, y_senter, er_y=True, sc=skala)
                 elif sym == "kryss": self.tegn_iof_kryss(self.bilde, x_sym3, y_senter, er_y=False, sc=skala)
+                elif sym == "taarn": self.tegn_iof_taarn(self.bilde, x_sym3, y_senter, sc=skala)
+                elif sym == "kunstig": self.tegn_iof_kunstig(self.bilde, x_sym3, y_senter, sc=skala)
+                elif sym == "tre": self.tegn_iof_tre(self.bilde, x_sym3, y_senter, sc=skala)
 
                 x_sym4 = start_x + celle_w * 3 + int(celle_w/2)
                 if ret == "fot": self.tegn_iof_fot(self.bilde, x_sym4, y_senter, sc=skala)
@@ -276,6 +317,31 @@ class MapRenderer:
                 elif ret in ["N", "S", "E", "W", "NW", "NE", "SW", "SE"]:
                     self.tegn_iof_retning(self.bilde, x_sym4, y_senter, ret, sc=skala)
 
-    # Ligger her i bunnen som den skal!
-    def lagre(self, filnavn):
-        cv2.imwrite(filnavn, self.bilde)
+    # --- NYTT: Legg til Passepartout (Ramme og Tittel) ---
+    def fullfoer_design(self, tittel, undertekst):
+        h, w = self.bilde.shape[:2]
+        marg_topp = 120
+        marg_bunn = 60
+        marg_side = 60
+        
+        # Lag et hvitt lerret som er litt større enn kartet
+        lerret = np.full((h + marg_topp + marg_bunn, w + marg_side*2, 3), 255, dtype=np.uint8)
+        # Lim inn kartet i midten
+        lerret[marg_topp:marg_topp+h, marg_side:marg_side+w] = self.bilde
+        
+        # Tegn en stilig svart ramme rundt selve kartbildet
+        cv2.rectangle(lerret, (marg_side, marg_topp), (marg_side+w, marg_topp+h), (0,0,0), 3)
+        self.bilde = lerret
+        
+        # Bruk PIL for å skrive stilig overskrift
+        self.start_tekst_lag()
+        self.tegn_tekst(tittel, (marg_side, int(marg_topp * 0.45)), 1.5, (0,0,0), tykkelse=3)
+        self.tegn_tekst(undertekst, (marg_side, int(marg_topp * 0.8)), 0.6, (100,100,100), tykkelse=1)
+        self.bruk_tekst_lag()
+
+    # --- NYTT: PDF Eksport ---
+    def lagre_som_pdf(self, filnavn):
+        img_rgb = cv2.cvtColor(self.bilde, cv2.COLOR_BGR2RGB)
+        pil_img = Image.fromarray(img_rgb)
+        # Lagrer som PDF i høy oppløsning!
+        pil_img.save(filnavn, "PDF", resolution=300.0)
